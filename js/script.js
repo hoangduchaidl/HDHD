50af8fd88cbcd42dbc08e92d125f48f45e2ffdd6
hoangduchaidl <147085493+hoangduchaidl@users.noreply.github.com>
2023-10-06 00:31:00 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,458 @@
+(function($) {
+
+	"use strict";
+
+
+    /*------------------------------------------
+        = FUNCTIONS
+    -------------------------------------------*/
+
+
+    // Toggle mobile navigation
+    function toggleMobileNavigation() {
+        var navbar = $(".navigation-holder");
+        var openBtn = $(".navbar-header .open-btn");
+        var closeBtn = $(".navigation-holder .close-navbar");
+        var navLinks = $("#navbar > ul > li > a[href^='#']");
+
+        openBtn.on("click", function() {
+            if (!navbar.hasClass("slideInn")) {
+                navbar.addClass("slideInn");
+            }
+            return false;
+        })
+
+        closeBtn.on("click", function() {
+            if (navbar.hasClass("slideInn")) {
+                navbar.removeClass("slideInn");
+            }
+            return false;
+        })
+
+        navLinks.on("click", function() {
+            if (navbar.hasClass("slideInn")) {
+                navbar.removeClass("slideInn");
+            }
+            return false;
+        })
+    }
+
+    toggleMobileNavigation();
+
+
+    // Function for toggle a class for small menu
+    function toggleClassForSmallNav() {
+        var windowWidth = window.innerWidth;
+        var mainNav = $("#navbar > ul");
+
+        if (windowWidth <= 991) {
+            mainNav.addClass("small-nav");
+        } else {
+            mainNav.removeClass("small-nav");
+        }
+    }
+
+    toggleClassForSmallNav();
+
+
+    // Function for small menu
+    function smallNavFunctionality() {
+        var windowWidth = window.innerWidth;
+        var mainNav = $(".navigation-holder");
+        var smallNav = $(".navigation-holder > .small-nav");
+        var subMenu = smallNav.find(".sub-menu");
+        var megamenu = smallNav.find(".mega-menu");
+        var menuItemWidthSubMenu = smallNav.find(".menu-item-has-children > a");
+
+        if (windowWidth <= 991) {
+            subMenu.hide();
+            megamenu.hide();
+            menuItemWidthSubMenu.on("click", function(e) {
+                var $this = $(this);
+                $this.siblings().slideToggle();
+                 e.preventDefault();
+                e.stopImmediatePropagation();
+            })
+        } else if (windowWidth > 991) {
+            mainNav.find(".sub-menu").show();
+            mainNav.find(".mega-menu").show();
+        }
+    }
+
+    smallNavFunctionality();
+
+
+    // function for active menuitem
+    function activeMenuItem($links) {
+        var top = $(window).scrollTop(),
+            windowHeight = $(window).height(),
+            documentHeight = $(document).height(),
+            cur_pos = top + 2,
+            sections = $("section"),
+            nav = $links,
+            nav_height = nav.outerHeight();
+
+
+        sections.each(function() {
+            var top = $(this).offset().top - nav_height,
+                bottom = top + $(this).outerHeight();
+
+            if (cur_pos >= top && cur_pos <= bottom) {
+                nav.find("> ul > li > a").parent().removeClass("current-menu-item");
+                nav.find("a[href='#" + $(this).attr('id') + "']").parent().addClass("current-menu-item");
+            } else if (cur_pos === 2) {
+                nav.find("> ul > li > a").parent().removeClass("current-menu-item");
+            }
+
+        });
+    }
+
+
+    // smooth-scrolling
+    function smoothScrolling($scrollLinks, $topOffset) {
+        var links = $scrollLinks;
+        var topGap = $topOffset;
+
+        links.on("click", function() {
+            if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $("[name=" + this.hash.slice(1) +"]");
+                if (target.length) {
+                    $("html, body").animate({
+                    scrollTop: target.offset().top - topGap
+                }, 1000, "easeInOutExpo");
+                    return false;
+                }
+            }
+            return false;
+        });
+    }
+
+
+   
+
+
+    // Hero slider background setting
+    function sliderBgSetting() {
+        if ($(".hero-slider .slide-item").length) {
+            $(".hero-slider .slide-item").each(function() {
+                var $this = $(this);
+                var img = $this.find(".slider-bg").attr("src");
+
+                $this.css({
+                    backgroundImage: "url("+ img +")",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center center"
+                })
+            });
+        }
+    }
+
+
+
+
+    // set two coloumn height equial
+    function setTwoColEqHeight($col1, $col2) {
+        var firstCol = $col1,
+            secondCol = $col2,
+            firstColHeight = $col1.innerHeight(),
+            secondColHeight = $col2.innerHeight();
+
+        if (firstColHeight > secondColHeight) {
+            secondCol.css({
+                "height": firstColHeight + 1 + "px"
+            })
+        } else {
+            firstCol.css({
+                "height": secondColHeight + 1 + "px"
+            })
+        }
+    }
+
+    function popupSaveTheDateCircle() {
+        var saveTheDateCircle = $(".save-the-date");
+        saveTheDateCircle.addClass("popup-save-the-date");
+    }
+
+
+
+    /*------------------------------------------
+        = HIDE PRELOADER
+    -------------------------------------------*/
+    function preloader() {
+        if($('.preloader').length) {
+            $('.preloader').delay(1).fadeOut("fast", function() {
+
+                //active wow
+                wow.init();
+
+                if($(".save-the-date").length) {
+                    popupSaveTheDateCircle();
+                }
+            });
+        }
+    }
+
+
+    /*------------------------------------------
+        = Icon động của ngày cưới
+    -------------------------------------------*/
+    var wow = new WOW({
+        boxClass:     'wow',      // default
+        animateClass: 'animated', // default
+        offset:       0,          // default
+        mobile:       true,       // default
+        live:         true        // default
+    });
+
+
+    /*------------------------------------------
+        = hiện ảnh khi ấn vào
+    -------------------------------------------*/
+    if ($(".gallery-fancybox").length) {
+        $(".fancybox").fancybox({
+            openEffect  : "elastic",
+            closeEffect : "elastic",
+            wrapCSS     : "project-fancybox-title-style"
+        });
+    }
+
+
+
+    /*------------------------------------------
+        = hiện popup map, video, youtube
+    -------------------------------------------*/
+    $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
+
+
+
+
+
+
+    /*------------------------------------------
+        = FUNCTION FORM SORTING GALLERY
+    -------------------------------------------*/
+    function sortingGallery() {
+        if ($(".sortable-gallery .gallery-filters").length) {
+            var $container = $('.gallery-container');
+            $container.isotope({
+                filter:'*',
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false,
+                }
+            });
+
+            $(".gallery-filters li a").on("click", function() {
+                $('.gallery-filters li .current').removeClass('current');
+                $(this).addClass('current');
+                var selector = $(this).attr('data-filter');
+                $container.isotope({
+                    filter:selector,
+                    animationOptions: {
+                        duration: 750,
+                        easing: 'linear',
+                        queue: false,
+                    }
+                });
+                return false;
+            });
+        }
+    }
+
+    sortingGallery();
+
+
+    /*------------------------------------------
+        = MASONRY GALLERY SETTING
+    -------------------------------------------*/
+    function masonryGridSetting() {
+        if ($('.masonry-gallery').length) {
+            var $grid =  $('.masonry-gallery').masonry({
+                itemSelector: '.grid',
+                columnWidth: '.grid',
+                percentPosition: true
+            });
+
+            $grid.imagesLoaded().progress( function() {
+                $grid.masonry('layout');
+            });
+        }
+    }
+
+    masonryGridSetting();
+
+
+    /*------------------------------------------
+        = Thanh ngang header
+    -------------------------------------------*/
+
+    // Function for clone an element for sticky menu
+    function cloneNavForSticyMenu($ele, $newElmClass) {
+        $ele.addClass('original').clone().insertAfter($ele).addClass($newElmClass).removeClass('original');
+    }
+
+    // clone home style 1 navigation for sticky menu
+    if ($('.header-style-1 .navigation').length) {
+        cloneNavForSticyMenu($('.header-style-1 .navigation'), "sticky");
+    }
+
+    // clone home style 1 navigation for sticky menu
+    if ($('.header-style-2 .navigation').length) {
+        cloneNavForSticyMenu($('.header-style-2 .navigation'), "sticky-2");
+    }
+
+    // Function for sticky menu
+    function stickIt($stickyClass, $toggleClass, $topOffset) {
+        if ($(window).scrollTop() >= $topOffset) {
+            var orgElement = $(".original");
+            var widthOrgElement = orgElement.css("width");
+
+            $stickyClass.addClass($toggleClass);
+
+            $stickyClass.css({
+                "width": widthOrgElement
+            }).show();
+
+            $(".original").css({
+                "visibility": "hidden"
+            });
+
+        } else {
+
+            $(".original").css({
+                "visibility": "visible"
+            });
+
+            $stickyClass.removeClass($toggleClass);
+        }
+    }
+
+
+    /*-------------------------------------------------------
+        = Cô dâu - chú rể
+    -----------------------------------------------------*/
+    if ($(".wedding-couple-section .gb").length) {
+
+        var imgHolder = $(".wedding-couple-section .gb .img-holder");
+
+        imgHolder.each(function() {
+            var $this = $(this);
+            var imgHolderPic = $this.find("img").attr("src");
+
+            $this.css({
+                backgroundImage: "url("+ imgHolderPic +")",
+                backgroundSize: "cover",
+                backgroundPosition: "center center"
+            })
+        })
+    }
+
+
+    /*------------------------------------------
+        = COUNTDOWN CLOCK
+    -------------------------------------------*/
+    if ($("#clock").length) {
+        $('#clock').countdown('2022/10/30', function(event) {
+            var $this = $(this).html(event.strftime(''
+            + '<div class="box"><div>%D</div> <span>Days</span> </div>'
+            + '<div class="box"><div>%H</div> <span>Hours</span> </div>'
+            + '<div class="box"><div>%M</div> <span>Mins</span> </div>'
+            + '<div class="box"><div>%S</div> <span>Secs</span> </div>'));
+        });
+    }
+
+
+    /*------------------------------------------
+        = STORY SLIDER
+    -------------------------------------------*/
+    if ($(".story-slider").length) {
+        $('.story-slider').owlCarousel({
+            items: 1,
+            dots: false,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            smartSpeed: 1000,
+            loop: true,
+        });
+    }
+
+
+   
+
+   
+    /*------------------------------------------
+        = SURFACE SHADER
+    -------------------------------------------*/
+    if ($(".surface-shader").length) {
+        //$('.surface-shader')
+    }
+
+
+
+     /*==========================================================================
+        WHEN DOCUMENT LOADING
+    ==========================================================================*/
+        $(window).on('load', function() {
+
+            preloader();
+
+            sliderBgSetting();
+
+            toggleMobileNavigation();
+
+            smallNavFunctionality();
+
+            //set the couple section groom bride two col equal height
+            if($(".wedding-couple-section").length) {
+                setTwoColEqHeight($(".wedding-couple-section .gb .img-holder"), $(".wedding-couple-section .gb .details"));
+            }
+
+            smoothScrolling($("#navbar > ul > li > a[href^='#']"), $(".header-style-1 .navigation").innerHeight());
+
+        });
+
+
+
+    /*==========================================================================
+        WHEN WINDOW SCROLL
+    ==========================================================================*/
+    $(window).on("scroll", function() {
+
+        bgParallax();
+
+        activeMenuItem($(".navigation-holder"));
+
+        if ($(".header-style-1").length) {
+            stickIt($(".sticky"), "sticky-on", $(".header-style-1 .navigation").offset().top);
+        }
+
+        if ($(".header-style-2").length) {
+            stickIt($(".sticky-2"), "sticky-on", 300);
+        }
+    });
+
+
+    /*==========================================================================
+        WHEN WINDOW RESIZE
+    ==========================================================================*/
+    $(window).on("resize", function() {
+        toggleClassForSmallNav();
+        //smallNavFunctionality();
+
+        clearTimeout($.data(this, 'resizeTimer'));
+        $.data(this, 'resizeTimer', setTimeout(function() {
+            smallNavFunctionality();
+        }, 200));
+    });
+
+
+
+})(window.jQuery);
